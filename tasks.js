f0f448955bd76af8e824190ac420cb6d9fa37954
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-03-13 09:06:40 +0100

    JQUERY

--- tasks.js
+++ tasks.js
@@ -132,7 +132,7 @@ function loadTasksIntoHTML() {
 function loadToDoTask(task) {
     let newTask = document.createElement('li');
     newTask.classList.add('mt-3');
-    newTask.innerHTML = '<a id="draggable" class="block p-5 rounded-lg shadow bg-white" href="#">\n' +
+    newTask.innerHTML = '<a draggable="true" class="block p-5 rounded-lg shadow bg-white" href="#">\n' +
         '                                <div class="flex justify-between">\n' +
         '                                    <p class="text-sm w-48 font-medium leading-snug text-gray-900">' + task.title + '</p>\n' +
         '                                    <span>\n' +
